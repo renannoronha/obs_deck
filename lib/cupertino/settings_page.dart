c4ab201c9109b51d8799084cbda5cbec7439cc44
Renan Noronha <renancnoronha@gmail.com>
2020-09-30 16:32:12 -0300

    cupertino theme button

--- lib/cupertino/settings_page.dart
+++ lib/cupertino/settings_page.dart
@@ -3,6 +3,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter_redux/flutter_redux.dart';
 import 'package:obs_deck/redux/app/app_state.dart';
 import 'package:obs_deck/redux/settings/settings_actions.dart';
+import 'package:obs_deck/themes.dart';
 
 class SettingsPage extends StatelessWidget {
   @override
@@ -126,8 +127,7 @@ class SettingsPage extends StatelessWidget {
                     child: CupertinoButton(
                       color: state.settingsState.studioButtonColor,
                       onPressed: () {
-                        StoreProvider.of<AppState>(context)
-                            .dispatch(ToggleStudioAction(!state.settingsState.studioMode));
+                        StoreProvider.of<AppState>(context).dispatch(ToggleStudioAction(!state.settingsState.studioMode));
                       },
                       child: Container(
                         height: 50,
@@ -135,6 +135,49 @@ class SettingsPage extends StatelessWidget {
                         child: Center(
                           child: Text(
                             state.settingsState.studioButtonLabel,
+                            style: TextStyle(fontSize: 20),
+                            textAlign: TextAlign.center,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(top: 5, left: 10, right: 10, bottom: 5),
+                    child: CupertinoButton(
+                      // color: Color.fromRGBO(199, 199, 199, 1),
+                      onPressed: () {
+                        final act = CupertinoActionSheet(
+                            title: Text('Select a Theme'),
+                            // message: Text('Which option?'),
+                            actions: <Widget>[
+                              CupertinoActionSheetAction(
+                                child: Text('Light'),
+                                onPressed: () {
+                                  StoreProvider.of<AppState>(context).dispatch(ChangeThemeAction('Light'));
+                                },
+                              ),
+                              CupertinoActionSheetAction(
+                                child: Text('Dark'),
+                                onPressed: () {
+                                  StoreProvider.of<AppState>(context).dispatch(ChangeThemeAction('Dark'));
+                                },
+                              ),
+                            ],
+                            cancelButton: CupertinoActionSheetAction(
+                              child: Text('Cancel'),
+                              onPressed: () {
+                                Navigator.pop(context);
+                              },
+                            ));
+                        showCupertinoModalPopup(context: context, builder: (BuildContext context) => act);
+                      },
+                      child: Container(
+                        height: 50,
+                        width: double.infinity,
+                        child: Center(
+                          child: Text(
+                            state.settingsState.theme,
                             style: TextStyle(fontSize: 20),
                             textAlign: TextAlign.center,
                           ),
